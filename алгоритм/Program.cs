48a83402cef0f4158e3173e92d42e95259d6b0f2
alikc <0pnessst@gmail.com>
2023-03-10 11:50:39 +0300

    added first part work

---
+++ алгоритм/Program.cs
@@ -0,0 +1,32 @@
+﻿// написать программу, которая из имеющегося
+// массива строк формируется массив из строк,
+// длина которых меньше либо равна 3 символа
+
+Console.Clear();
+
+string[] CreatArrayFromArray(string[] array, int maximumChar)
+{
+    int numberOfNumbers = array.Length;
+    int index = 0;
+    int indexSmaller = 0;
+    while (index < numberOfNumbers)
+    {
+        if (array[index].Length <= maximumChar)
+        {
+            indexSmaller++;
+        }
+        index++;
+    }
+    string[] result = new string[indexSmaller];
+    index = 0;
+    indexSmaller = 0;
+    while (index < numberOfNumbers)
+    {
+        if (array[index].Length <= maximumChar)
+        {
+            result[indexSmaller++] = array[index];
+        }
+        index++;
+    }
+    return result;
+}
