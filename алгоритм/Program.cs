3f5cb3e44572b7061c9b59f08672d01fc6549763
alikc <0pnessst@gmail.com>
2023-03-10 12:11:48 +0300

    added second part work

--- алгоритм/Program.cs
+++ алгоритм/Program.cs
@@ -30,3 +30,7 @@ string[] CreatArrayFromArray(string[] array, int maximumChar)
     }
     return result;
 }
+
+string[] massiv = new string[10] {"Dip", "abcdefj", "010", "green", "in", "yolia", "1a", "8", "nine", "ten"};
+string[] secondMassiv = CreatArrayFromArray(massiv, 3);
+Console.WriteLine(string.Join(", ", secondMassiv));
